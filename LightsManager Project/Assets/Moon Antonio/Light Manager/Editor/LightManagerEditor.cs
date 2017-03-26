1234883f91ee842bf014289de6b74331171f0292
Moon Antonio <pinchodev91@gmail.com>
2017-03-26 21:39:52 +0200

    Light tools add

--- LightsManager Project/Assets/Moon Antonio/Light Manager/Editor/LightManagerEditor.cs
+++ LightsManager Project/Assets/Moon Antonio/Light Manager/Editor/LightManagerEditor.cs
@@ -10,6 +10,7 @@
 #region Librerias
 using UnityEngine;
 using UnityEditor;
+using System;
 using System.Collections.Generic;
 #endregion
 
@@ -68,10 +69,42 @@ namespace MoonAntonio
 		/// </summary>
 		private void OnGUI()// Interfaz de LightManagerEditor
 		{
+			EditorGUILayout.BeginVertical("box");
+
+			EditorGUILayout.BeginHorizontal("box");
+			GUILayout.Button("Lights");
+			GUILayout.Button("Reclection Probes");
+			GUILayout.Button("Lights Probes");
+			GUILayout.Button("Light shafts");
+			EditorGUILayout.EndHorizontal();
+
+
+			EditorGUILayout.BeginHorizontal("box");
+
+			EditorGUILayout.LabelField("Estado", GUILayout.MinWidth(100), GUILayout.Width(50));
+			EditorGUILayout.LabelField("Nombre", GUILayout.MinWidth(100), GUILayout.Width(140));
+			EditorGUILayout.LabelField("Tipo", GUILayout.MinWidth(100), GUILayout.Width(100));
+			EditorGUILayout.LabelField("Modo", GUILayout.MinWidth(100), GUILayout.Width(100));
+			EditorGUILayout.LabelField("Color", GUILayout.MinWidth(100), GUILayout.Width(50));
+			EditorGUILayout.LabelField("Intensidad", GUILayout.MinWidth(100), GUILayout.Width(200));
+
+			EditorGUILayout.EndHorizontal();
+
 			for (int n = 0; n < luces.Count; n++)
 			{
-				EditorGUILayout.LabelField(luces[n].name);
+				EditorGUILayout.BeginHorizontal("box");
+
+				luces[n].enabled = EditorGUILayout.Toggle(luces[n].enabled, GUILayout.MinWidth(100), GUILayout.Width(50));
+				luces[n].name = EditorGUILayout.TextField(luces[n].name, GUILayout.MinWidth(100), GUILayout.Width(140));
+				luces[n].type = (LightType)EditorGUILayout.EnumPopup(luces[n].type, GUILayout.MinWidth(100), GUILayout.Width(100));
+				luces[n].lightmappingMode = (LightmappingMode)EditorGUILayout.EnumPopup(luces[n].lightmappingMode, GUILayout.MinWidth(100), GUILayout.Width(100));
+				luces[n].color = EditorGUILayout.ColorField(luces[n].color, GUILayout.MinWidth(100), GUILayout.Width(50));
+				luces[n].intensity = EditorGUILayout.Slider(luces[n].intensity,0,10, GUILayout.MinWidth(100), GUILayout.Width(200));
+
+				EditorGUILayout.EndHorizontal();
 			}
+
+			EditorGUILayout.EndVertical();
 		}
 		#endregion
 
@@ -81,6 +114,11 @@ namespace MoonAntonio
 		/// </summary>
 		private void Escaneo()// Escanea las luces de la escena
 		{
+			// Limpiar lista y array
+			Array.Clear(lights, 0, lights.Length);
+			luces.Clear();
+
+			// Agregar las luces a la lista
 			lights = FindObjectsOfType(typeof(Light)) as Light[];
 			foreach (Light light in lights)
 			{
