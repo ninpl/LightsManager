acf07f96f46b70ac263c098caa8c2bb4c29e5875
Moon Antonio <pinchodev91@gmail.com>
2017-03-26 22:31:17 +0200

    Finish Tool

--- LightsManager Project/Assets/Moon Antonio/Light Manager/Editor/LightManagerEditor.cs
+++ LightsManager Project/Assets/Moon Antonio/Light Manager/Editor/LightManagerEditor.cs
@@ -19,7 +19,7 @@ namespace MoonAntonio
 	/// <summary>
 	/// <para>Herramienta para controlar las luces de al escena</para>
 	/// </summary>
-	[ExecuteInEditMode]
+	//[ExecuteInEditMode]
 	public class LightManagerEditor : EditorWindow
 	{
 		#region Variables Publicas
@@ -27,7 +27,6 @@ namespace MoonAntonio
 		/// <para>Lista de luces en la escena.</para>
 		/// </summary>
 		public List<Light> luces = new List<Light>();               // Lista de luces en la escena
-
 		/// <summary>
 		/// <para>Lista de las reflection probes en la escena</para>
 		/// </summary>
@@ -74,6 +73,21 @@ namespace MoonAntonio
 			// Escaneo de las luces de la escena
 			Escaneo();
 		}
+
+		public void OnHierarchyChange()
+		{
+			//Escaneo();
+		}
+
+		public void OnInspectorUpdate()
+		{
+			if (lights.Length > 0) Array.Clear(lights, 0, lights.Length);
+			if (reflec.Length > 0) Array.Clear(reflec, 0, reflec.Length);
+			if (luces.Count > 0) luces.Clear();
+			if (reflection.Count > 0) reflection.Clear();
+
+			Escaneo();
+		}
 		#endregion
 
 		#region UI
@@ -86,73 +100,69 @@ namespace MoonAntonio
 
 			EditorGUILayout.BeginHorizontal("box");
 			if (GUILayout.Button("Lights")) estadoherramienta = 0;
-			if (GUILayout.Button("Reclection Probes")) estadoherramienta = 1;
-#if LIGHTSHAFTS
-			if (GUILayout.Button("Light shafts")) estadoherramienta = 2;
-#endif
+			if (GUILayout.Button("Reflection Probes")) estadoherramienta = 1;
 			EditorGUILayout.EndHorizontal();
 
 			switch (estadoherramienta)
 			{
 				#region Luces
 				case 0:
-					EditorGUILayout.BeginHorizontal("box");
-
-					EditorGUILayout.LabelField("Estado", GUILayout.MinWidth(100), GUILayout.Width(50));
-					EditorGUILayout.LabelField("Nombre", GUILayout.MinWidth(100), GUILayout.Width(140));
-					EditorGUILayout.LabelField("Tipo", GUILayout.MinWidth(100), GUILayout.Width(100));
-					EditorGUILayout.LabelField("Modo", GUILayout.MinWidth(100), GUILayout.Width(100));
-					EditorGUILayout.LabelField("Color", GUILayout.MinWidth(100), GUILayout.Width(50));
-					EditorGUILayout.LabelField("Intensidad", GUILayout.MinWidth(100), GUILayout.Width(200));
-
-					EditorGUILayout.EndHorizontal();
-
-					for (int n = 0; n < luces.Count; n++)
+					if (luces.Count > 0)
 					{
 						EditorGUILayout.BeginHorizontal("box");
 
-						luces[n].enabled = EditorGUILayout.Toggle(luces[n].enabled, GUILayout.MinWidth(100), GUILayout.Width(50));
-						luces[n].name = EditorGUILayout.TextField(luces[n].name, GUILayout.MinWidth(100), GUILayout.Width(140));
-						luces[n].type = (LightType)EditorGUILayout.EnumPopup(luces[n].type, GUILayout.MinWidth(100), GUILayout.Width(100));
-						luces[n].lightmappingMode = (LightmappingMode)EditorGUILayout.EnumPopup(luces[n].lightmappingMode, GUILayout.MinWidth(100), GUILayout.Width(100));
-						luces[n].color = EditorGUILayout.ColorField(luces[n].color, GUILayout.MinWidth(100), GUILayout.Width(50));
-						luces[n].intensity = EditorGUILayout.Slider(luces[n].intensity, 0, 10, GUILayout.MinWidth(100), GUILayout.Width(200));
+						EditorGUILayout.LabelField("Estado", GUILayout.MinWidth(100), GUILayout.Width(50));
+						EditorGUILayout.LabelField("Nombre", GUILayout.MinWidth(100), GUILayout.Width(140));
+						EditorGUILayout.LabelField("Tipo", GUILayout.MinWidth(100), GUILayout.Width(100));
+						EditorGUILayout.LabelField("Modo", GUILayout.MinWidth(100), GUILayout.Width(100));
+						EditorGUILayout.LabelField("Color", GUILayout.MinWidth(100), GUILayout.Width(50));
+						EditorGUILayout.LabelField("Intensidad", GUILayout.MinWidth(100), GUILayout.Width(200));
 
 						EditorGUILayout.EndHorizontal();
+
+						for (int n = 0; n < luces.Count; n++)
+						{
+							EditorGUILayout.BeginHorizontal("box");
+
+							luces[n].enabled = EditorGUILayout.Toggle(luces[n].enabled, GUILayout.MinWidth(100), GUILayout.Width(50));
+							luces[n].name = EditorGUILayout.TextField(luces[n].name, GUILayout.MinWidth(100), GUILayout.Width(140));
+							luces[n].type = (LightType)EditorGUILayout.EnumPopup(luces[n].type, GUILayout.MinWidth(100), GUILayout.Width(100));
+							luces[n].lightmappingMode = (LightmappingMode)EditorGUILayout.EnumPopup(luces[n].lightmappingMode, GUILayout.MinWidth(100), GUILayout.Width(100));
+							luces[n].color = EditorGUILayout.ColorField(luces[n].color, GUILayout.MinWidth(100), GUILayout.Width(50));
+							luces[n].intensity = EditorGUILayout.Slider(luces[n].intensity, 0, 10, GUILayout.MinWidth(100), GUILayout.Width(200));
+
+							EditorGUILayout.EndHorizontal();
+						}
 					}
 					break;
 				#endregion
 
-				#region Reclection Probes
+				#region Reflection Probes
 				case 1:
-					EditorGUILayout.BeginHorizontal("box");
-
-					EditorGUILayout.LabelField("Estado", GUILayout.MinWidth(100), GUILayout.Width(50));
-					EditorGUILayout.LabelField("Nombre", GUILayout.MinWidth(100), GUILayout.Width(140));
-					EditorGUILayout.LabelField("Tipo", GUILayout.MinWidth(100), GUILayout.Width(100));
-					EditorGUILayout.LabelField("Intensidad", GUILayout.MinWidth(100), GUILayout.Width(200));
-
-					EditorGUILayout.EndHorizontal();
-
-					for (int n = 0; n < reflection.Count; n++)
+					if (reflection.Count > 0)
 					{
 						EditorGUILayout.BeginHorizontal("box");
 
-						reflection[n].enabled = EditorGUILayout.Toggle(reflection[n].enabled, GUILayout.MinWidth(100), GUILayout.Width(50));
-						reflection[n].name = EditorGUILayout.TextField(reflection[n].name, GUILayout.MinWidth(100), GUILayout.Width(140));
-						reflection[n].mode = (UnityEngine.Rendering.ReflectionProbeMode)EditorGUILayout.EnumPopup(reflection[n].mode, GUILayout.MinWidth(100), GUILayout.Width(100));
-						reflection[n].intensity = EditorGUILayout.Slider(reflection[n].intensity, 0, 10, GUILayout.MinWidth(100), GUILayout.Width(200));
+						EditorGUILayout.LabelField("Estado", GUILayout.MinWidth(100), GUILayout.Width(50));
+						EditorGUILayout.LabelField("Nombre", GUILayout.MinWidth(100), GUILayout.Width(140));
+						EditorGUILayout.LabelField("Tipo", GUILayout.MinWidth(100), GUILayout.Width(100));
+						EditorGUILayout.LabelField("Intensidad", GUILayout.MinWidth(100), GUILayout.Width(200));
 
 						EditorGUILayout.EndHorizontal();
+
+						for (int n = 0; n < reflection.Count; n++)
+						{
+							EditorGUILayout.BeginHorizontal("box");
+
+							reflection[n].enabled = EditorGUILayout.Toggle(reflection[n].enabled, GUILayout.MinWidth(100), GUILayout.Width(50));
+							reflection[n].name = EditorGUILayout.TextField(reflection[n].name, GUILayout.MinWidth(100), GUILayout.Width(140));
+							reflection[n].mode = (UnityEngine.Rendering.ReflectionProbeMode)EditorGUILayout.EnumPopup(reflection[n].mode, GUILayout.MinWidth(100), GUILayout.Width(100));
+							reflection[n].intensity = EditorGUILayout.Slider(reflection[n].intensity, 0, 10, GUILayout.MinWidth(100), GUILayout.Width(200));
+
+							EditorGUILayout.EndHorizontal();
+						}
 					}
 					break;
-				#endregion
-
-				#region Lights Probes
-#if LIGHTSHAFTS
-				case 2:
-					break;
-#endif
 				#endregion
 
 				default:
@@ -161,6 +171,7 @@ namespace MoonAntonio
 			}
 
 			EditorGUILayout.EndVertical();
+
 		}
 		#endregion
 
@@ -170,25 +181,32 @@ namespace MoonAntonio
 		/// </summary>
 		private void Escaneo()// Escanea las luces de la escena
 		{
-			// Limpiar lista y array
-			Array.Clear(lights, 0, lights.Length);
-			Array.Clear(reflec, 0, reflec.Length);
-			luces.Clear();
-			reflection.Clear();
-
-
 			// Agregar las luces a la lista
 			lights = FindObjectsOfType(typeof(Light)) as Light[];
-			foreach (Light light in lights)
+			if (lights.Length > 0)
 			{
-				luces.Add(light);
+				foreach (Light light in lights)
+				{
+					luces.Add(light);
+				}
+			}
+			else
+			{
+				estadoherramienta = 1;
 			}
 
 			// Agregar las reclection a la lista
 			reflec = FindObjectsOfType(typeof(ReflectionProbe)) as ReflectionProbe[];
-			foreach (ReflectionProbe reflectionP in reflec)
+			if (reflec.Length > 0)
 			{
-				reflection.Add(reflectionP);
+				foreach (ReflectionProbe reflectionP in reflec)
+				{
+					reflection.Add(reflectionP);
+				}
+			}
+			else
+			{
+				estadoherramienta = 0;
 			}
 		}
 		#endregion
