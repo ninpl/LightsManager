5a7e626e81dd038ae400a0e090e37beba745fdda
Moon Antonio <pinchodev91@gmail.com>
2017-03-26 20:36:07 +0200

    Menu item add

---
+++ LightsManager Project/Assets/Moon Antonio/Editor/LightManagerEditor.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// LightManagerEditor.cs (26/03/2017)											\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Herramienta para controlar las luces de al escena.			\\
+// Fecha Mod:		26/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEditor;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Herramienta para controlar las luces de al escena</para>
+	/// </summary>
+	[ExecuteInEditMode]
+	public class LightManagerEditor : Editor 
+	{
+
+		[MenuItem("Moon Antonio/ManagerLight",false,1)]
+		public static void Init()
+		{
+
+		}
+	}
+}
